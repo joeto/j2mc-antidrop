9fa918f2a09b4a0fd7855ce94c57fbb7093f54fd
Matt Baxter <matt@phozop.net>
2012-04-05 03:06:20 -0400

    Updates

--- src/to/joe/j2mc/antidrop/J2MC_Antidrop.java
+++ src/to/joe/j2mc/antidrop/J2MC_Antidrop.java
@@ -2,28 +2,33 @@ package to.joe.j2mc.antidrop;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class J2MC_Antidrop extends JavaPlugin implements Listener{
-    
+public class J2MC_Antidrop extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
         this.getLogger().info("Anti drops module enabled");
     }
-    
+
     @Override
     public void onDisable() {
         this.getLogger().info("Anti drops module disabled");
     }
-    
-    
+
     @EventHandler
-    public void onDropEvent(PlayerDropItemEvent event){
-        if(!event.getPlayer().hasPermission("j2mc.core.admin")){
-            event.setCancelled(true);
-        }
+    public void onItemEvent(ItemSpawnEvent event) {
+        event.setCancelled(true);
     }
     
+    @EventHandler
+    public void onEggThrow(PlayerEggThrowEvent event){
+        if(!event.getPlayer().hasPermission("j2mc.core.admin")){
+           event.setHatching(false); 
+        }
+    }
+
 }
