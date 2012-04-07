e081dd2dbbde701cb2c720bd105ab7d6103759a2
Matt Baxter <matt@phozop.net>
2012-04-06 23:56:11 -0400

    No fun

--- src/to/joe/j2mc/antidrop/J2MC_Antidrop.java
+++ src/to/joe/j2mc/antidrop/J2MC_Antidrop.java
@@ -2,7 +2,9 @@ package to.joe.j2mc.antidrop;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,6 +30,14 @@ public class J2MC_Antidrop extends JavaPlugin implements Listener {
     public void onEggThrow(PlayerEggThrowEvent event){
         if(!event.getPlayer().hasPermission("j2mc.core.admin")){
            event.setHatching(false); 
+        }
+    }
+    
+    @EventHandler
+    public void spawn(CreatureSpawnEvent event){
+        SpawnReason reason=event.getSpawnReason();
+        if(reason==SpawnReason.BUILD_IRONGOLEM || reason==SpawnReason.BUILD_SNOWMAN){
+            event.setCancelled(true);
         }
     }
 
