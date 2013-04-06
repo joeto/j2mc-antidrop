e5c6a67cd795caf889496122f1c613627ec5d462
Ivan Panarusky <eviltechie@joe.to>
2013-04-05 22:23:08 -0400

    removed silly code

--- src/to/joe/j2mc/antidrop/J2MC_Antidrop.java
+++ src/to/joe/j2mc/antidrop/J2MC_Antidrop.java
@@ -5,8 +5,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,12 +16,6 @@ public class J2MC_Antidrop extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
-        this.getLogger().info("Anti drops module enabled");
-    }
-
-    @Override
-    public void onDisable() {
-        this.getLogger().info("Anti drops module disabled");
     }
 
     @EventHandler
